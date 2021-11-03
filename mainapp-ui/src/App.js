e58088194943a9e63660459ce57607c9ac80da78
TAVALKS <AlekseiTetyev@gmail.com>
2021-11-03 12:01:45 +0500

    этап bootstrap

--- mainapp-ui/src/App.js
+++ mainapp-ui/src/App.js
@@ -2,28 +2,13 @@ import logo from './logo.svg';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const [people, setPeople] = useState([])
-
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url : "http://127.0.0.1:8080/api/test-api/"
-    }).then(response => {
-      setPeople(response.data)
-    })
-  },[])
-  
   return (
     <div className="App">
       <h1>Hello from React to Django</h1>
-      <ul>
-        {people.map(p =>(
-          <li key={p.id}>{p.name}</li>
-        ))}
-      </ul>
     </div>
   );
 }
