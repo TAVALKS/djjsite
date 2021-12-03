809fec603718e28d10d43dfd35a36895c40108a3
TAVALKS <AlekseiTetyev@gmail.com>
2021-12-03 16:19:39 +0500

    добавлена строка

--- mainapp-ui/src/App.js
+++ mainapp-ui/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navigation/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   );
 }
