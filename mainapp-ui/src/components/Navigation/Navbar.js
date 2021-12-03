6731c398eb9b84f123f28d38aa2f60bd73e1c755
TAVALKS <AlekseiTetyev@gmail.com>
2021-12-03 14:57:20 +0500

    demo for navbar

--- mainapp-ui/src/components/Navigation/Navbar.js
+++ mainapp-ui/src/components/Navigation/Navbar.js
@@ -4,12 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
 
-    const [categories, setCategories] = useEffect([])
+    const [categories, setCategories] = useState([])
 
     useEffect(()=>{
         axios({
-            method: 'GET',
-            url: 'http://127.0.0.1:8000/api/category/'
+            method: "GET",
+            url: 'http://127.0.0.1:8080/api/category/'
         }).then(response => {
             setCategories(response.data)
         })
@@ -19,37 +19,18 @@ function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
             <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
                 <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                    <li><hr className="dropdown-divider"/></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-                </li>
+                {categories.map(c =>(
+                    <a className="nav-link" href="#" key={c.id}>{c.name}</a>
+                ))}
             </ul>
-            <form className="d-flex">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
             </div>
         </div>
         </nav>
