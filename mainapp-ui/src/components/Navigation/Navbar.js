0a7e7a297e7959703c9f3a025c4547e0df9dca65
TAVALKS <AlekseiTetyev@gmail.com>
2021-12-10 14:02:16 +0500

    added center text on navbar

--- mainapp-ui/src/components/Navigation/Navbar.js
+++ mainapp-ui/src/components/Navigation/Navbar.js
@@ -19,12 +19,12 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#" ></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav mx-auto">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
