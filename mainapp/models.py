4bc0b4bf151a07adeafc6357683a73fee8ea84d4
TAVALKS <AlekseiTetyev@gmail.com>
2022-01-11 16:51:37 +0500

    added ordering in Meta priority

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,13 +7,14 @@ class BlogCategory(models.Model):
 
     name = models.CharField(max_length=255, verbose_name='Имя категории')
     slug = models.SlugField(unique = True)
-    priority = models.IntegerField(unique=True, null=True)
+    priority = models.IntegerField(unique=True, null=True, verbose_name='Приоритет для показа на фронте')
 
 
     def __str__(self):
         return self.name
 
     class Meta:
+        ordering = ['priority']
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
