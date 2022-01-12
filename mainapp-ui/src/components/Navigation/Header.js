5cd20dfd168a9a67f3ef856cfc8f01751b07c6bd
TAVALKS <AlekseiTetyev@gmail.com>
2022-01-12 11:50:17 +0500

    added header

---
+++ mainapp-ui/src/components/Navigation/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+
+    return (
+        <nav class="navbar navbar-light bg-light mx-auto">
+            <div class="container-fluid mx-auto">
+                <a class="navbar-brand mx-auto" href="#">
+                Проектирование и внедрение голосовых роботов
+                </a>
+            </div>
+        </nav>
+  );
+}
+
+export default Header;
