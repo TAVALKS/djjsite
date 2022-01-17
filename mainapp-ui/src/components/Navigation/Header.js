8468cf2ff0b17a884ae6a755017e5fc615f0e4c5
TAVALKS <AlekseiTetyev@gmail.com>
2022-01-17 17:16:33 +0500

    added new info

--- mainapp-ui/src/components/Navigation/Header.js
+++ mainapp-ui/src/components/Navigation/Header.js
@@ -4,7 +4,7 @@ function Header() {
         <nav class="navbar navbar-light bg-light mx-auto">
             <div class="container-fluid mx-auto">
                     <h4 className="navbar-brand mx-auto" href="#" >
-                        Проектирование и внедрение голосовых роботов
+                        Написание текстов, песен, музыки на заказ
                     </h4>
                     <h4 className="navbar-brand mx-auto" href="#" >
                         9-9999-999-999
